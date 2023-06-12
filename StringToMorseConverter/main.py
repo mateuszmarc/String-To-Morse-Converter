98aad87bf68aef199418cc97b6554a378fa886ec
Mateusz Marcykiewicz <mmarcykiewicz@gmail.com>
2023-06-12 18:36:27 +0000

    update main.py for program functionality and create readme.txt file

--- StringToMorseConverter/main.py
+++ StringToMorseConverter/main.py
@@ -1,1 +1,10 @@
+import morse_converter
+import morse_dictionary
+import program_handler
+
+morse_convert =\
+    morse_converter.MorseConverter(morse_dictionary.char_to_morse_dictionary)
+
+program_manager = program_handler.ProgramHandler(morse_convert)
+program_manager.execute_request()
 
---
+++ StringToMorseConverter/readme.txt
@@ -0,0 +1,7 @@
+Program description:
+
+Program asks the user to choose the activity he wants program to perform.
+It can convert message made of ASCII characters to Morse code
+and play it. And also it can convert Morse message to message made
+of ASCII characters.
+
